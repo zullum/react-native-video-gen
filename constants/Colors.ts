f63a50d249223ba4d7dcf3a8ba8a74f6175b156b
Sanel Zulic <zullumsan@gmail.com>
2025-01-29 11:43:21 +0100

    Initial commit

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,26 +1,30 @@
-/**
- * Below are the colors that are used in the app. The colors are defined in the light and dark mode.
- * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
- */
-
-const tintColorLight = '#0a7ea4';
-const tintColorDark = '#fff';
-
 export const Colors = {
-  light: {
-    text: '#11181C',
+    primary: '#dc4c3e',
+    secondary: '#D88E2E',
     background: '#fff',
-    tint: tintColorLight,
-    icon: '#687076',
-    tabIconDefault: '#687076',
-    tabIconSelected: tintColorLight,
-  },
-  dark: {
-    text: '#ECEDEE',
-    background: '#151718',
-    tint: tintColorDark,
-    icon: '#9BA1A6',
-    tabIconDefault: '#9BA1A6',
-    tabIconSelected: tintColorDark,
-  },
-};
+    backgroundAlt: '#f5f5f5',
+    dark: '#635E5E',
+    lightText: '#a6a6a6',
+    lightBorder: '#d9d9d9',
+  };
+  
+  export const DATE_COLORS = {
+    today: '#2f9d23',
+    tomorrow: '#9d6023',
+    weekend: '#233d9d',
+    other: '#54239d',
+  };
+  
+  export const PROJECT_COLORS = [
+    '#0079bf',
+    '#d29034',
+    '#519839',
+    '#b04632',
+    '#89609e',
+    '#cd5a91',
+    '#4bbf6b',
+    '#00aecc',
+    '#838c91',
+  ];
+  
+  export const DEFAULT_PROJECT_COLOR = PROJECT_COLORS[0];
